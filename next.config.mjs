75aa6f40c92777cd1f8b6c8988791ed781ab2b53
Karabo Seipelo <1788640+Karabo-Seipelo@users.noreply.github.com>
2024-09-11 22:47:50 +0200

    Refactor next.config.mjs to remove unnecessary options

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/2024",
-    output: 'export',
-    reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
